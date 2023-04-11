6e767239ab5086dc8935686410c8af629ba1e6db
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-04-11 13:41:39 +0600

    project python link shortner online update

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,6 @@ from .views import python_link_shortener_online
 
 urlpatterns = [
     
-    path('python_link_shortener_online/', python_link_shortener_online, name='python_link_shortener_online'),
+    path('python-link-shortener-online/', python_link_shortener_online, name='python_link_shortener_online'),
     
 ]
